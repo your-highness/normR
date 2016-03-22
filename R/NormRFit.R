243488242a5bf779e701e0c8d5e5d4cfcf95aea3
your-highness <hiob@noisecore.org>
2016-03-22 19:29:08 +0100

    Removing trailing whitespaces

--- R/NormRFit.R
+++ R/NormRFit.R
@@ -1,78 +1,78 @@
 # Copyright (C) 2016 Johannes Helmuth
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #' Container for a NormR fit
 #'
-#' This S4 class wraps a NormR fit containing counts, fit configuration and 
-#' results of the fit. 
+#' This S4 class wraps a NormR fit containing counts, fit configuration and
+#' results of the fit.
 #'
 #' This class provides printing, summary and accessor methods for retrieving
 #' data from the object. Internally this class uses a map structure which saves
 #' memory, such that accessing data through \code{slot()} gives undesired
-#' output. Please use accessor methods. A NormRFit object is read-only, i.e. it 
+#' output. Please use accessor methods. A NormRFit object is read-only, i.e. it
 #' can not be modified.
 #'
 #' @param obj A NormRFit object.
 #' @param i \code{integer()}-index for subsetting.
 #' @param B The index of the background component.
 #'
-#' @slot type A \code{character()} representing the type of fit ("enrichR", 
+#' @slot type A \code{character()} representing the type of fit ("enrichR",
 #' "diffR", "regimeR")
 #' @slot n Number of regions analyzed in total.
 #' @slot ranges A \code{GenomicRanges}-object specifying the analysed regions.
-#' @slot k An \code{integer()} giving the number of binomial mixture 
+#' @slot k An \code{integer()} giving the number of binomial mixture
 #' components to be fit to the data.
 #' @slot B The index of the background component.
 #' @slot map A \code{integer}-vector to map back \code{counts},
-#' \code{lnposteriors}, \code{lnenrichment}, \code{lnpvals} and \code{lnqvals}. 
+#' \code{lnposteriors}, \code{lnenrichment}, \code{lnpvals} and \code{lnqvals}.
 #' See also \code{\link{map2uniquePairs()}} for map generation.
-#' @slot counts A \code{list()} of length 2 containing unique count pairs. Use 
-#' accessor \code{\link{getCounts}} to retrieve original count matrix. 
+#' @slot counts A \code{list()} of length 2 containing unique count pairs. Use
+#' accessor \code{\link{getCounts}} to retrieve original count matrix.
 #' @slot names The corresponding names for counts.
-#' @slot thetaStar A \code{numeric()} representing a naive background 
+#' @slot thetaStar A \code{numeric()} representing a naive background
 #' estimation, i.e. \code{sum(getCounts(obj)[2,])/sum(getCounts(obj))}
-#' @slot theta A \code{k}-length \code{numeric()}-vector representing 
+#' @slot theta A \code{k}-length \code{numeric()}-vector representing
 #' NormR-fitted parametrization of binomial mixture components.
-#' @slot mixtures A \code{k}-length \code{numeric()}-vector representing 
+#' @slot mixtures A \code{k}-length \code{numeric()}-vector representing
 #' NormR-fitted mixture proportions of binomial mixture
 #' components.
 #' @slot lnL A \code{numeric()}-vector representing the log-likelihood-trace of
 #' the NormR model fit.
-#' @slot eps A \code{numeric{}} representing preset threshold for NormR fit 
+#' @slot eps A \code{numeric{}} representing preset threshold for NormR fit
 #' convergence.
 #' @slot lnposteriors A \code{k}-dimensional \code{matrix()} containing ln
-#' posterior probabilities for every unique \code{n} 2-tupel in counts given 
+#' posterior probabilities for every unique \code{n} 2-tupel in counts given
 #' each \code{theta} and \code{mixtures}. Use accessor
 #' \code{\link{getPosterior()}} to get the complete posterior matrix.
-#' @slot lnenrichment A \code{numeric()}-vector of normalized ln enrichment over 
+#' @slot lnenrichment A \code{numeric()}-vector of normalized ln enrichment over
 #' component \code{B} for each unique count-tupel. Use accessor
 #' \code{\link{getEnrichment}} to retrieve enrichment for original count sequence.
-#' @slot lnpvals A \code{numeric()}-vector containing ln P-values every unique 
+#' @slot lnpvals A \code{numeric()}-vector containing ln P-values every unique
 #' 2-tupel in counts given \code{theta} of \code{B}.
 #' @slot filteredT A \code{integer()}-vector giving indices of P-values to be
 #' considered for FDR correction. These indices have been obtainted by filtering
 #' P-values using the T method.
-#' @slot lnqvals A \code{numeric()}-vector containing ln q-values. These are 
+#' @slot lnqvals A \code{numeric()}-vector containing ln q-values. These are
 #' P-values corrected for multiple testing using Storey's method.
 #' @aliases NormRFit
-#' @seealso \code{\link{normr-methods}} for the functions that produce 
+#' @seealso \code{\link{normr-methods}} for the functions that produce
 #' this object
 #' @return return values are described in the Methods section.
 #' @example inst/examples/class_example.R
 #' @export
-setClass("NormRFit", 
+setClass("NormRFit",
   representation = representation(type = "character",
                                   n = "integer",
                                   ranges = "GenomicRanges",
@@ -109,21 +109,21 @@ setValidity("NormRFit",
         length(object@counts[[1]]) != length(object@counts[[2]])) {
       return("invalid counts slot")
     }
-    if (length(object@thetastar) != 1 || object@thetastar < 0 || 
+    if (length(object@thetastar) != 1 || object@thetastar < 0 ||
         object@thetastar > 1) {
       return("invalid thetastar slot")
     }
     if (length(object@theta) != object@k) return("invald theta slot")
     if (length(object@mixtures) != object@k) return("invald mixtures slot")
     n <- length(object@counts[[1]])
-    if (NCOL(object@lnposteriors) != object@k || 
+    if (NCOL(object@lnposteriors) != object@k ||
         NROW(object@lnposteriors) != length(object@counts[[1]])) {
       return("invald lnposterios slot")
     }
     if (length(object@lnenrichment) != length(object@counts[[1]])) {
       return("invaled lnenrichment slot")
     }
-    if (length(object@lnpvals) != length(object@counts[[1]])) { 
+    if (length(object@lnpvals) != length(object@counts[[1]])) {
       return("invaled lnpvals slot")
     }
     if (max(object@filteredT) > length(object@counts[[1]])) {
@@ -143,7 +143,7 @@ setMethod("print", "NormRFit",
         "Number of Regions:\t", x@n, "\n",
         "Theta* (naive bg):\t", format(x@thetastar,digits=digits), "\n\n")
     if (length(x@theta)) {
-      cat("Results of fit\n", 
+      cat("Results of fit\n",
           "Mixture Proporitons:\n")
       print.default(format(x@mixtures,digits=digits), print.gap=2L, quote=F)
       cat("Theta:\n")
@@ -154,7 +154,7 @@ setMethod("print", "NormRFit",
     cat("\n")
     invisible(x)
   }
-) 
+)
 setMethod("show", "NormRFit", function(object) print(object))
 
 setMethod("plot", "NormRFit",
@@ -173,13 +173,13 @@ setMethod("length", "NormRFit", function(x) x@n)
 #' @param ... Not used
 #' @return NULL
 #' @export
-setMethod("summary", "NormRFit", 
+setMethod("summary", "NormRFit",
   function(object, print=T, digits=3, ...) {
     ans <- paste0("NormRFit-class object\n\n",
                   "Type:\t\t'", object@type, "'\n",
                   "Number of Regions:\t", object@n, "\n",
                   "Number of components:\t", object@k, "\n",
-                  "Theta* (naive bg):\t", 
+                  "Theta* (naive bg):\t",
                   format(object@thetastar, digits=digits), "\n",
                   "Backgroundcomponent B:\t", object@B, "\n\n")
     cat(ans)
@@ -189,7 +189,7 @@ setMethod("summary", "NormRFit",
       cat("\nTheta:\n")
       print.default(format(object@theta,digits=digits), print.gap=2L, quote=F)
       cat("\nBayesian Information Criterion:\t", format(
-        (-2*object@lnL[length(object@lnL)]+length(object@theta)*log(object@n)), 
+        (-2*object@lnL[length(object@lnL)]+length(object@theta)*log(object@n)),
         digits=digits), "\n\n")
       cat("Significantly different from background B:\n")
       cts <- c("***"=length(which(object@lnqvals[object@map] <= log(0))),
@@ -211,14 +211,14 @@ setGeneric("getCounts", function(obj) standardGeneric("getCounts"))
 #' @describeIn NormRFit Retrieve used counts.
 #' @aliases getCounts
 #' @export
-setMethod("getCounts", "NormRFit", function(obj) { 
-  list("control"=obj@counts[[1]][obj@map], 
+setMethod("getCounts", "NormRFit", function(obj) {
+  list("control"=obj@counts[[1]][obj@map],
        "treatment"=obj@counts[[2]][obj@map])
 })
 
 #'@export
 setGeneric("getEnrichment", function(obj) standardGeneric("getEnrichment"))
-#' @describeIn NormRFit Retrieve NormR calculated enrichment. Ln enrichment for 
+#' @describeIn NormRFit Retrieve NormR calculated enrichment. Ln enrichment for
 #' a difference call.
 #' @aliases getEnrichment
 #' @export
@@ -269,16 +269,16 @@ setMethod("getQvalues", "NormRFit", function(obj) {
 })
 
 #' @export
-setGeneric("writeEnrichment", function(obj,...) 
+setGeneric("writeEnrichment", function(obj,...)
   standardGeneric("writeEnrichment"))
 #' describeIn NormRFit Writes the calculated enrichment of a NormRFit to disk.
 #' @param obj A NormRFit object
 #' @param filename A file to write to.
-#' @param gr A GenomicRanges object representing the analyzed data. Usually, 
+#' @param gr A GenomicRanges object representing the analyzed data. Usually,
 #' ranges are stored inside \code{obj}. Use this if you want to change
 #' chromosome identifiers for example. Note that the width of the bins should be
 #' same to data and fit stored in \code{obj}.
-#' 
+#'
 #' @aliases writeEnrichment
 #' @export
 setMethod("writeEnrichment", "NormRFit",
@@ -301,17 +301,17 @@ setMethod("writeEnrichment", "NormRFit",
 )
 
 #' @export
-setGeneric("writeBed", function(obj,...) 
+setGeneric("writeBed", function(obj,...)
   standardGeneric("writeBed"))
 #' describeIn NormRFit Writes a bed file of regions called significant for
 #' @param obj A NormRFit object
 #' @param filename A file to write to.
 #' @param pval A p-value threshold.
-#' @param gr A GenomicRanges object representing the analyzed data. Usually, 
+#' @param gr A GenomicRanges object representing the analyzed data. Usually,
 #' ranges are stored inside \code{obj}. Use this if you want to change
 #' chromosome identifiers for example. Note that the width of the bins should be
 #' same to data and fit stored in \code{obj}.
-#' 
+#'
 #' @aliases writeBed
 #' @export
 setMethod("writeBed", "NormRFit",
