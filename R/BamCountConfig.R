7786d6159890196db275da75bb7a3604073d21e6
your-highness <helmuth@molgen.mpg.de>
2016-03-22 19:29:08 +0100

    Removing trailing whitespaces

--- R/BamCountConfig.R
+++ R/BamCountConfig.R
@@ -1,15 +1,15 @@
 # Copyright (C) 2016 Johannes Helmuth
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -18,37 +18,37 @@
 #' This S4 class is a small wrapper for a configuration on obtaining counts
 #' from bamfiles with bamsignals::bamProfile().
 #'
-#' @slot type A \code{character()} representing the type of bamfile 
+#' @slot type A \code{character()} representing the type of bamfile
 #' ("paired.end", "single.end").
 #' @param chromosomes GenomicRanges object used to specify the chromosomes.
 #' @slot binsize An \code{integer()} giving the binsize to count in (in bp).
-#' @slot mapqual discard reads with mapping quality strictly lower than this 
-#' parameter. The value 0 ensures that no read will be discarded, the value 254 
+#' @slot mapqual discard reads with mapping quality strictly lower than this
+#' parameter. The value 0 ensures that no read will be discarded, the value 254
 #' that only reads with the highest possible mapping quality will be considered.
 #' @slot filteredFlag An \code{integer()} to filter for when counting reads (SAMFLAG).
 #' For example, 1024 filters out marked duplicates (default), see
 #' \url{https://broadinstitute.github.io/picard/explain-flags.html}
-#' @slot shift shift the read position by a user-defined number of basepairs. 
+#' @slot shift shift the read position by a user-defined number of basepairs.
 #' This can be handy in the analysis of chip-seq data.
-#' @slot midpoint A \code{logical()} indicating whether fragment midpoints 
+#' @slot midpoint A \code{logical()} indicating whether fragment midpoints
 #' instead of 5'-ends should be counted in paired end data.
-#' @slot tlenfilter A filter on fragment length as estimated from alignment 
-#' in paired end experiments (TLEN). If set to \code{c(min,max)} only reads are 
-#' considered where \code{min <= TLEN <= max}. If \code{paired.end=="ignore"}, 
-#' this argument is set to \code{NULL} and no filtering is done. If 
+#' @slot tlenfilter A filter on fragment length as estimated from alignment
+#' in paired end experiments (TLEN). If set to \code{c(min,max)} only reads are
+#' considered where \code{min <= TLEN <= max}. If \code{paired.end=="ignore"},
+#' this argument is set to \code{NULL} and no filtering is done. If
 #' \code{paired.end!="ignore"}, this argument defaults to \code{c(0,1000)}.
 #'
 #' @aliases BamCountConfig
 #' @aliases BamsignalsConfig
 #; @aliases BamsignalsCountConfig
-#' 
+#'
 #' @import GenomicRanges
-#' @seealso \code{\link{normr-methods}} for the functions that require this 
+#' @seealso \code{\link{normr-methods}} for the functions that require this
 #' object.\cr
 #' See \code{\link{bamsignals}}-package for counting in bam files.
 #' @return return values are described in the Methods section.
 #' @export
-setClass("BamCountConfig", 
+setClass("BamCountConfig",
     representation = representation(type="character",
                                     chromosomes="GenomicRanges",
                                     binsize="integer",
@@ -61,7 +61,7 @@ setClass("BamCountConfig",
 )
 
 setValidity("BamCountConfig",
-    function(object) { 
+    function(object) {
       if (!(object@type %in% c("single.end", "paired.end"))) {
         stop("invalid type")
       }
@@ -106,10 +106,10 @@ setGeneric("countConfigPairedEnd", function(...)
 #' @describeIn BamCountConfig Setup a paired end counting configuration
 #' @aliases countConfigPairedEnd
 #' @export
-setMethod("countConfigPairedEnd", 
+setMethod("countConfigPairedEnd",
   definition=function(binsize=250, mapq=20, filteredFlag=1024, shift=0, midpoint=T,
                       tlenfilter=c(70, 200)) {
-    new("BamCountConfig", type="paired.end", binsize=binsize, mapq=mapq, 
+    new("BamCountConfig", type="paired.end", binsize=binsize, mapq=mapq,
         filteredFlag=filteredFlag, shift=shift, midpoint=midpoint,
         tlenfilter=tlenfilter)
 })
@@ -120,15 +120,15 @@ setGeneric("countConfigSingleEnd", function(...)
 #' @describeIn BamCountConfig Setup a single end counting configuration
 #' @aliases countConfigSingleEnd
 #' @export
-setMethod("countConfigSingleEnd", 
+setMethod("countConfigSingleEnd",
   definition=function(binsize=250, mapq=20, filteredFlag=1024, shift=0) {
-    new("BamCountConfig", type="single.end", binsize=binsize, mapq=mapq, 
+    new("BamCountConfig", type="single.end", binsize=binsize, mapq=mapq,
         filteredFlag=filteredFlag, shift=shift)
 })
 
 #' @export
 setGeneric("getFilter", function(obj) standardGeneric("getFilter"))
-#' @describeIn BamCountConfig 
+#' @describeIn BamCountConfig
 #' @aliases getFilter Get the bamsignals relevant filter
 #' @export
 setMethod("getFilter", "BamCountConfig",
