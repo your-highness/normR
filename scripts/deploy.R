332ac37638f08e5be251d6b6c4110e135caf4512
Johannes Helmuth <helmuth@molgen.mpg.de>
2016-03-24 10:12:09 +0100

    Updated deploy script

--- scripts/deploy.R
+++ scripts/deploy.R
@@ -10,8 +10,55 @@
 #
 # mammana, helmuth 2015-01-13
 
+registerFunctions <- function (pkg_name, prefix="") {
+  if (missing(pkg_name))
+    pkg_name <- basename(getwd())
+  files <- list.files("src", pattern = "[cC]$", full.names = TRUE)
+  files <- files[files != paste0("src/", pkg_name, "_init.c")]
+  c_files <- lapply(files, readLines)
+  get_c_prototypes <- function(x) {
+    export_lines <- grep("// \\[\\[export\\]\\]", x)
+    sapply(export_lines, function(line) {
+           return(gsub("(.*)// \\[\\[export\\]\\]\n(.*?) ?\\{(.*)",
+                       "\\2;", paste(x[line:length(x)], collapse = "\n")))
+  })}
+  c_prototypes <- sapply(c_files, get_c_prototypes)
+  if (length(c_prototypes) != 0)
+    c_prototypes <- c_prototypes[sapply(c_prototypes, function(x) {
+                                        !identical(x, list()) })]
+  rcpp_exports <- readLines("src/RcppExports.cpp")
+  fn_lines <- grep("^RcppExport", rcpp_exports, value = TRUE)
+  cpp_prototypes <- sapply(fn_lines, USE.NAMES = FALSE, function(x) {
+                           gsub("RcppExport (.*) \\{", "\\1;", x)  })
+  all_prototypes <- unlist(c(c_prototypes, cpp_prototypes))
+  all_names <- sapply(all_prototypes, function(x) {
+                      gsub("SEXP (.*)\\(.*", "\\1", x)})
+  all_nargs <- sapply(all_prototypes, function(x) {
+                      defn <- gsub("SEXP (.*)\\((.*)\\).*", "\\2", x)
+                      m <- gregexpr("SEXP +", defn)
+                      if (identical(as.integer(m[[1]]), -1L)) {
+                        return(0)
+                      }
+                      else {
+                        return(length(m[[1]]))
+                      }})
+  Cnames <- paste0(prefix, all_names)
+  cmd_lines <- paste0("{\"", Cnames, "\", (DL_FUNC) &", all_names,
+                      ", ", all_nargs, "},")
+  R_CallMethodsDef <- c("R_CallMethodDef callMethods[]  = {",
+                        paste0("  ", cmd_lines), "  {NULL, NULL, 0}", "};")
+  R_RegisterRoutines <- c(paste0("void R_init_", pkg_name,
+                                 "(DllInfo *info) {"), "  R_registerRoutines(info, NULL, callMethods, NULL, NULL);",
+                          "  R_useDynamicSymbols(info, FALSE);", "}")
+  init.c <- c("#include <R.h>", "#include <Rinternals.h>",
+              "", "#include <R_ext/Rdynload.h>", "", all_prototypes,
+              "", R_CallMethodsDef, "", R_RegisterRoutines, "")
+  cat(init.c, file = paste0("src/", pkg_name, "_init.c"), sep = "\n")
+  return(invisible(NULL))
+}
+
 #action
-deployR <- function(pckg=".") {
+deployR <- function(pckg=".", registerFun=T, check=T) {
   chgDir <- F
   if (pckg == ".") {#assuming current directory
     pckg <- tail(strsplit(getwd(), split="/")[[1]], 1)
@@ -27,9 +74,7 @@ deployR <- function(pckg=".") {
     stop("Package directory does not exist.")
   }
 
-
   message("Deploying ", pckg, "...")
-
   vsion <- read.dcf(file.path(pckg, "DESCRIPTION"))[,"Version"]
   message("Version: ", vsion)
 
@@ -40,8 +85,9 @@ deployR <- function(pckg=".") {
   roxygenize(pckg, clean=T)
 
   setwd(pckg)
-  require(Kmisc)
-  Kmisc:::registerFunctions(pckg, prefix="")
+  if (registerFun) {
+    registerFunctions(pckg, prefix="")
+  }
 
   library(devtools)
   build_vignettes()
@@ -53,7 +99,11 @@ deployR <- function(pckg=".") {
   #  try installing the package in a stub library
   tmp.lib <- paste0(pckg, "_BUILDtmp")
   dir.create(tmp.lib) 
-  system(paste0("R3 CMD build ", pckg))
+  if (check) {
+    system(paste0("~/R-devel/bin/R CMD check ", pckg))
+    system(paste0("~/R-devel/bin/R CMD BiocCheck ", pckg))
+  }
+  system(paste0("~/R-devel/bin/R CMD build ", pckg))
   install.packages(paste0(pckg, "_", vsion, ".tar.gz"), lib=tmp.lib)
   library(pckg, character.only=T)
   unlink(tmp.lib, recursive=T)
