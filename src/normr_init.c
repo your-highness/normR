8b5f42e70bbd5076f87346825d18617c81d27474
your-highness <helmuth@molgen.mpg.de>
2016-05-21 13:13:54 +0200

    Bioc required C files

--- src/normr_init.c
+++ src/normr_init.c
@@ -7,7 +7,7 @@ SEXP normr_logRowSum(SEXP matSEXP, SEXP nthreadsSEXP);
 SEXP normr_logSumVector(SEXP vecSEXP, SEXP nthreadsSEXP);
 SEXP normr_mapToOriginal(SEXP vecSEXP, SEXP m2uSEXP);
 SEXP normr_mapToUniqueWithMap(SEXP vecSEXP, SEXP m2uSEXP);
-SEXP normr_computeEnrichment(SEXP rSEXP, SEXP sSEXP, SEXP lnPostSEXP, SEXP thetaSEXP, SEXP FSEXP, SEXP BSEXP, SEXP diffCallSEXP, SEXP nthreadsSEXP);
+SEXP normr_computeEnrichmentWithMap(SEXP lnPostSEXP, SEXP m2uSEXP, SEXP thetaSEXP, SEXP FSEXP, SEXP BSEXP, SEXP diffCallSEXP, SEXP nthreadsSEXP);
 SEXP normr_normr_core(SEXP rSEXP, SEXP sSEXP, SEXP modelsSEXP, SEXP epsSEXP, SEXP iterationsSEXP, SEXP bgIdxSEXP, SEXP diffCallSEXP, SEXP verboseSEXP, SEXP nthreadsSEXP);
 
 R_CallMethodDef callMethods[]  = {
@@ -15,7 +15,7 @@ R_CallMethodDef callMethods[]  = {
   {"normr_logSumVector", (DL_FUNC) &normr_logSumVector, 2},
   {"normr_mapToOriginal", (DL_FUNC) &normr_mapToOriginal, 2},
   {"normr_mapToUniqueWithMap", (DL_FUNC) &normr_mapToUniqueWithMap, 2},
-  {"normr_computeEnrichment", (DL_FUNC) &normr_computeEnrichment, 8},
+  {"normr_computeEnrichmentWithMap", (DL_FUNC) &normr_computeEnrichmentWithMap, 7},
   {"normr_normr_core", (DL_FUNC) &normr_normr_core, 9},
   {NULL, NULL, 0}
 };
