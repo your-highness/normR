27c54cea819705057a1e651f21a95b98a88210e2
your-highness <hiob@noisecore.org>
2016-06-15 15:43:45 +0200

    Vignette identation, BamCountConfig renaming. Numbers to be redone

--- inst/doc/normr.R
+++ inst/doc/normr.R
@@ -55,8 +55,10 @@ genome
 
 ## ----warning=FALSE-------------------------------------------------------
 #Enrichment Calling for H3K4me3 and H3K36me3
-k4me3Fit <- enrichR(treatment = k4me3Bamfile, control = inputBamfile, genome = genome, verbose = F)
-k36me3Fit <- enrichR(treatment = k36me3Bamfile, control = inputBamfile, genome = genome, verbose = F)
+k4me3Fit <- enrichR(treatment = k4me3Bamfile, control = inputBamfile,
+                    genome = genome, verbose = F)
+k36me3Fit <- enrichR(treatment = k36me3Bamfile, control = inputBamfile,
+                     genome = genome, verbose = F)
 
 ## ------------------------------------------------------------------------
 k4me3Fit
@@ -79,26 +81,47 @@ idx <- which(rowSums(do.call(cbind, getCounts(k4me3Fit))) != 0)
 summary(k4me3Enr[idx])
 
 ## ----fig.width=10, fig.height=10-----------------------------------------
-x <- getEnrichment(k4me3Fit)[idx]
+x <- k4me3Enr[idx]
 y <- getEnrichment(k36me3Fit)[idx]
 d.x <- density(x); d.y <- density(y)
 limits <- range(x,y)
 layout( matrix( c(0,2,2,1,3,3,1,3,3),ncol=3) )
-plot(d.x$x, d.x$y, xlim=limits, type='l', main="H3K36me3 normalized Enrichment", xlab="", ylab="Density")
+plot(d.x$x, d.x$y, xlim=limits, type='l',
+     main="H3K36me3 normalized Enrichment", xlab="", ylab="Density")
 abline(v=0, lty=3, lwd=2, col=4)
-plot(d.y$y, d.y$x, ylim=limits, xlim=rev(range(d.y$y)), type='l', main="H3K4me3 normalized Enrichment", xlab="Density", ylab="")
+plot(d.y$y, d.y$x, ylim=limits, xlim=rev(range(d.y$y)), type='l',
+     main="H3K4me3 normalized Enrichment", xlab="Density", ylab="")
 abline(h=0, lty=3, lwd=2, col=4)
-plot(x, y, xlim=limits, ylim=limits, col=adjustcolor("grey10", alpha.f=.2), pch=20, xlab="", ylab="")
-abline(0, 1, lty=2, lwd=3, col=2); abline(v=0, lty=3, lwd=2, col=4); abline(h=0, lty=3, lwd=2, col=4);
+color <- rep("grey10", length(idx))
+plot(x, y, xlim=limits, ylim=limits, pch=20, xlab="", ylab="",
+     col=adjustcolor(color, alpha.f=.2))
+abline(0, 1, lty=2, lwd=3, col=2)
+abline(v=0, lty=3, lwd=2, col=4)
+abline(h=0, lty=3, lwd=2, col=4)
 
-## ------------------------------------------------------------------------
+## ----fig.width=10, fig.height=10-----------------------------------------
 #integer vector with <NA> set to non-significant regions
 k4me3Classes <- getClasses(k4me3Fit, fdr = 0.05)
+k36me3Classes <- getClasses(k36me3Fit, fdr = 0.05)
+
+#Color scatter plot based on enrichment
+color[!is.na(k4me3Classes[idx])] <- "#2C9500"
+color[!is.na(k36me3Classes[idx])] <- "#990099"
+color[!is.na(k4me3Classes+k36me3Classes)[idx]] <- "#971621"
+plot(x, y, xlim=limits, ylim=limits, pch=20,
+     col=adjustcolor(color, alpha.f=.5), xlab="H3K4me3 normalized Enrichment",
+     ylab="H3K36me3 normalized Enrichment")
+legend("topright", pch=20, col=unique(color), cex=.6, bg="white",
+  legend=c("Background", "H3K36me3 enriched", "H3K4me3 enriched",
+           "H3K4me3/K36me3 enriched")
+  )
+
+## ------------------------------------------------------------------------
 k4me3Ranges <- getRanges(k4me3Fit)[!is.na(k4me3Classes)]
 #Alternatively you can extract ranges without storing the class vector
 k4me3Ranges <- getRanges(k4me3Fit, fdr = 0.05)
 
-#as expected we get 577 regions
+#as expected we get 380 regions
 length(k4me3Ranges)
 
 ## ----warning=FALSE-------------------------------------------------------
@@ -106,7 +129,7 @@ length(k4me3Ranges)
 genes <- read.delim(file = system.file("extdata", "genes.bed", package="normr"),
                     header = F,
                     stringsAsFactors = F)
-library(GenomicRanges)
+library(GenomicRanges, verbose = F)
 genes <- GRanges(seqnames = genes[, 1],
                  ranges = IRanges(start = genes[, 2], end = genes[, 3]),
                  strand = genes[, 6],
@@ -168,7 +191,8 @@ k4k36Dif <- diffR(treatment = getCounts(k4me3Fit)$treatment,
                   genome    = getRanges(k4me3Fit),
                   verbose   = F)
 #<or> (unnecessarily) count again
-#k4k36Dif <- diffR(treatment = k4me3Bamfile, control = k36me3Bamfile, genome = genome, verbose = F)
+#k4k36Dif <- diffR(treatment = k4me3Bamfile, control = k36me3Bamfile,
+#                  genome = genome, verbose = F)
 
 #summary statistics
 summary(k4k36Dif)
@@ -178,7 +202,6 @@ exportR(k4k36Dif, filename = "k4k36Dif.bed", type = "bed", fdr = 0.05)
 exportR(k4k36Dif, filename = "k4k36Dif.bw", type = "bigWig")
 
 ## ----warning=FALSE-------------------------------------------------------
-#Four regimes on H3K4me3 uncover potential cell/allele heterogeneity for transcriptional initiation
 k4me3Regimes <- regimeR(treatment = getCounts(k4me3Fit)$treatment,
                          control   = getCounts(k4me3Fit)$control,
                          genome    = getRanges(k4me3Fit),
@@ -186,7 +209,7 @@ k4me3Regimes <- regimeR(treatment = getCounts(k4me3Fit)$treatment,
                          verbose   = F)
 summary(k4me3Regimes)
 
-#Three regimes on H3K36me3 discriminate genes of high and low expression
+## ----warning=FALSE-------------------------------------------------------
 k36me3Regimes <- regimeR(treatment = getCounts(k36me3Fit)$treatment,
                          control   = getCounts(k36me3Fit)$control,
                          genome    = getRanges(k36me3Fit),
@@ -204,14 +227,16 @@ exportR(k36me3Regimes, filename = "k36me3Regimes.bed", type = "bed", fdr = 0.05)
 #  # Consider only reads with Mapping Quality >= 20.
 #  # Filter reads for marked duplicates (e.g. with picard mark-duplicates)
 #  # Shift the counting position for a read 100 bp downstream.
-#  countConfigSE <- countConfigSingleEnd(binsize = 500, mapq = 20, filteredFlag = 1024, shift = 100)
+#  countConfigSE <- countConfigSingleEnd(binsize = 500, mapq = 20,
+#                                        filteredFlag = 1024, shift = 100)
 #  
 #  #Paired End:
 #  # Count in 500bp bins.
 #  # Consider only reads with Mapping Quality >= 30.
 #  # Count the midpoint of the aligned fragment instead of 5' ends.
-#  # Consider only reads corresponding to fragments with size spanning from 100 to 300bp
-#  countConfigPE <- countConfigPairdEnd(binsize = 500, mapq = 30, midpoint = T, tlenfilter = c(100, 300))
+#  # Consider only reads corresponding to fragments with size from 100 to 300bp
+#  countConfigPE <- countConfigPairdEnd(binsize = 500, mapq = 30, midpoint = T,
+#                                       tlenfilter = c(100, 300))
 #  
 #  #Plug in the counting configuration into normR, e.g. in enrichR()
 #  fit <- enrichR(treatment   = k4me3Bamfile,
@@ -223,25 +248,27 @@ exportR(k36me3Regimes, filename = "k36me3Regimes.bed", type = "bed", fdr = 0.05)
 #regions have identical size?
 all(width(promoters) == 4000)
 
-#count in predefined regions
-library(bamsignals)
-k4Counts <- bamCount(bampath = k4me3Bamfile, gr = promoters, verbose = F)
-inputCounts <- bamCount(bampath = inputBamfile, gr = promoters, verbose = F)
-
-#Fit only on promoters
-promotersFit <- enrichR(treatment = k4Counts, control = inputCounts, genome = promoters)
+## ----eval=FALSE----------------------------------------------------------
+#  #count in predefined regions
+#  library(bamsignals)
+#  k4Counts <- bamCount(bampath = k4me3Bamfile, gr = promoters, verbose = F)
+#  inputCounts <- bamCount(bampath = inputBamfile, gr = promoters, verbose = F)
+#  
+#  #Fit only on promoters
+#  promotersFit <- enrichR(treatment = k4Counts, control = inputCounts,
+#                          genome = promoters, verbose = F)
 
 ## ----eval=FALSE----------------------------------------------------------
-#  #call CNVs a sufficiently large bin size (e.g. 10kb)
 #  cnvs <- diffR(treatment   = treatmentInputBamfile,
 #                control     = controlInputBamfile,
 #                genome      = genome,
-#                countConfig = countConfigSingleEnd(binsize = 1e4))
+#                countConfig = countConfigSingleEnd(binsize = 2.5e4))
 #  
 #  #export the CNV calls
 #  exportR(cnvs, "CNVs.bed")
 #  
 #  #Filter previous ChIP-seq difference calls for CNVs
-#  idx <- which(countOverlaps(getRanges(diffFit, fdr = .05), getRanges(cnvs, fdr = .05)) == 0)
+#  ov <- countOverlaps(getRanges(diffFit, fdr = .05), getRanges(cnvs, fdr = .05))
+#  idx <- which(ov == 0)
 #  cnvCleanedGR <- getRanges(diffFit, fdr = .05)[idx]
 
